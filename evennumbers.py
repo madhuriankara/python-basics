21c181e85aa4a3fc8c9f5a6dcd9bc21c2accafb8
Madhuri A <madhuriankara21@gmail.com>
2024-10-29 23:20:35 -0400

    pandas basic

---
+++ evennumbers.py
@@ -0,0 +1,5 @@
+for i in range(20, 9, -1):
+    if i % 2 == 0:
+        print(i)
+
+            
---
+++ sum_of_numbers.py
@@ -0,0 +1,11 @@
+def sum_of_numbers(num_1, num_2, num_3, num_4, num_5):
+    total = num_1 + num_2 + num_3 + num_4 + num_5     
+    return total
+num_1 = 1
+num_2 = 2
+num_3 = 3
+num_4 = 4
+num_5 = 5
+result = sum_of_numbers(num_1, num_2, num_3, num_4, num_5)
+print(result)
+    
